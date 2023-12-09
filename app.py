4fb91c45603ebf10d44bcbb05f539c1835fe4a7c
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2023-12-09 13:27:40 +0300

    Progress

---
+++ app.py
@@ -0,0 +1,176 @@
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify, request
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization, hashes, padding
+
+
+class User:
+	def __init__(self):
+		self.private_key, self.public_key = self.generate_key_pair()
+		self.blockchain = Blockchain()
+
+	def generate_key_pair(self):
+		private_key = rsa.generate_private_key(
+				public_exponent=65537,
+				key_size=2048,
+				backend=default_backend()
+		)
+		public_key = private_key.public_key()
+		return private_key, public_key
+
+
+class Blockchain:
+
+	def __init__(self):
+		self.chain = []
+		self.current_transactions = {}
+		self.generate_genesis_block()
+
+	def generate_genesis_block(self):
+		self.create_block(proof=1, previous_hash='0')
+
+	def create_block(self, proof, previous_hash):
+		block = {
+					'index': len(self.chain) + 1,
+					'timestamp': str(datetime.datetime.now()),
+					'proof': proof,
+					'previous_hash': previous_hash,
+					'transactions': self.current_transactions,
+		}
+
+		self.current_transactions = {}
+		self.chain.append(block)
+		return block
+
+	def create_transaction(self, sender, recipient, amount, public_key):
+		if public_key not in self.current_transactions:
+			self.current_transactions[public_key] = []
+
+		self.current_transactions[public_key].append({
+			'sender': sender,
+			'recipient': recipient,
+			'amount': amount,
+		})
+
+	@property
+	def last_block(self):
+		return self.chain[-1]
+
+	def proof_of_work(self, previous_proof):
+		new_proof = 1
+		check_proof = False
+
+		while check_proof is False:
+			hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+			if hash_operation[:5] == '00000':
+				check_proof = True
+			else:
+				new_proof += 1
+
+		return new_proof
+
+	def hash(self, block):
+		encoded_block = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(encoded_block).hexdigest()
+
+	def chain_valid(self, chain):
+		previous_block = chain[0]
+		block_index = 1
+
+		while block_index < len(chain):
+			block = chain[block_index]
+			if block['previous_hash'] != self.hash(previous_block):
+				return False
+
+			previous_proof = previous_block['proof']
+			proof = block['proof']
+			hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+
+			if hash_operation[:5] != '00000':
+					return False
+			previous_block = block
+			block_index += 1
+
+		return True
+
+
+app = Flask(__name__)
+users = {}
+
+
+@app.route('/mine_block/<user_id>', methods=['GET'])
+def mine_block(user_id):
+	user = users.get(user_id)
+
+	if not user:
+		return jsonify({'message': 'User not found'}), 404
+
+	previous_block = user.blockchain.last_block
+	previous_proof = previous_block['proof']
+	proof = user.blockchain.proof_of_work(previous_proof)
+	previous_hash = user.blockchain.hash(previous_block)
+	user.blockchain.create_block(proof, previous_hash)
+
+	response = {'message': 'A block is MINED',
+					 'index': user.blockchain.last_block['index'],
+					 'timestamp': user.blockchain.last_block['timestamp'],
+					 'proof': user.blockchain.last_block['proof'],
+					 'previous_hash': user.blockchain.last_block['previous_hash'],
+					 'transactions': user.blockchain.last_block['transactions']
+					}
+
+	return jsonify(response), 200
+
+
+@app.route('/get_chain/<user_id>', methods=['GET'])
+def display_chain(user_id):
+	user = users.get(user_id)
+
+	if not user:
+		return jsonify({'message': 'User not found'}), 404
+
+	response = {'chain': user.blockchain.chain,
+					 'length': len(user.blockchain.chain)}
+	return jsonify(response), 200
+
+
+@app.route('/valid/<user_id>', methods=['GET'])
+def valid(user_id):
+	user = users.get(user_id)
+
+	if not user:
+		return jsonify({'message': 'User not found'}), 404
+
+	valid = user.blockchain.chain_valid(user.blockchain.chain)
+
+	if valid:
+		response = {'message': 'The Blockchain is valid.'}
+	else:
+		response = {'message': 'The Blockchain is not valid.'}
+	return jsonify(response), 200
+
+
+@app.route('/add_transaction/<user_id>', methods=['POST'])
+def add_transaction(user_id):
+	data = request.get_json()
+
+	required_fields = ['sender', 'recipient', 'amount']
+	if not all(field in data for field in required_fields):
+		return 'Missing fields', 400
+
+	user = users.get(user_id)
+
+	if not user:
+		return jsonify({'message': 'User not found'}), 404
+
+	user.blockchain.create_transaction(data['sender'], data['recipient'], data['amount'])
+
+	response = {'message': 'Transaction added to current transactions'}
+	return jsonify(response), 201
+
+
+if __name__ == "__main__":
+	 app.run(host='127.0.0.1', port=5000)
--- src/node/utils.py
+++
@@ -1,6 +0,0 @@
-from Crypto.Hash import SHA256
-
-def calculate_hash(data: bytes) -> str:
-    h = SHA256.new()
-    h.update(data)
-    return h.hexdigest()
